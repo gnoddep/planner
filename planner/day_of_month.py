bca897443317c28c5634b0d3725688d0f14a34f1
Peter Gnodde <peter@gnodde.org>
2020-08-27 12:00:24 +0200

    Offset negative day from the end of the month in DayOfMonth planner

--- planner/day_of_month.py
+++ planner/day_of_month.py
@@ -1,47 +1,46 @@
 import calendar
 import datetime
+from typing import Tuple
 
 from .planning import Planning
 
+LAST_DAY_OF_MONTH = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
 
 class DayOfMonth(Planning):
-    FIRST = 1
-    LAST = 0
-
-    def __init__(self, day_of_month: int):
-        if (day_of_month < 1 or day_of_month > 31) and day_of_month != self.LAST:
-            raise ValueError('day_of_month must be from 1 to 31')
-        self.__day_of_month = day_of_month
+    def __init__(self, day: int):
+        assert 1 <= abs(day) <= 31
+        self.__day = day
 
     @property
-    def day_of_month(self) -> int:
-        return self.__day_of_month
+    def day(self) -> int:
+        return self.__day
 
     def match(self, date: datetime.datetime) -> bool:
-        return date.day == self.__get_day_of_month(date)
+        return date.day == self.__get_day_of_month(date.year, date.month)
 
     def next(self, date: datetime.datetime) -> datetime.datetime:
-        day_of_month = self.__get_day_of_month(date)
-        if date.day < day_of_month <= self.__get_last_day_of_month(date):
-            return date.replace(day=day_of_month, hour=0, minute=0, second=0, microsecond=0)
+        year = date.year
+        month = date.month
+        day = date.day
 
-        date = self.__next_month(date.replace(day=1))
-        while self.__day_of_month > self.__get_last_day_of_month(date):
-            date = self.__next_month(date)
+        while not day < self.__get_day_of_month(year, month) <= self.__get_last_day_of_month(year, month):
+            day = 0
+            year, month = self.__next_month(year, month)
 
-        return date.replace(day=self.__get_day_of_month(date), hour=0, minute=0, second=0, microsecond=0)
+        return datetime.datetime(year=year, month=month, day=self.__get_day_of_month(year, month), tzinfo=date.tzinfo)
 
-    def __get_day_of_month(self, date: datetime.datetime) -> int:
-        if self.__day_of_month == self.LAST:
-            return self.__get_last_day_of_month(date)
-        return self.__day_of_month
-
-    @staticmethod
-    def __get_last_day_of_month(date: datetime.datetime) -> int:
-        return calendar.monthrange(date.year, date.month)[1]
+    def __get_day_of_month(self, year: int, month: int) -> int:
+        if self.__day < 0:
+            return self.__get_last_day_of_month(year, month) + self.__day + 1
+        return self.__day
 
     @staticmethod
-    def __next_month(date: datetime.datetime) -> datetime.datetime:
-        if date.month == 12:
-            return date.replace(year=date.year + 1, month=1)
-        return date.replace(month=date.month + 1)
+    def __get_last_day_of_month(year: int, month: int) -> int:
+        return LAST_DAY_OF_MONTH[month] + int(month == 2 and calendar.isleap(year))
+
+    @staticmethod
+    def __next_month(year: int, month: int) -> Tuple[int, int]:
+        if month == 12:
+            return year + 1, 1
+        return year, month + 1
