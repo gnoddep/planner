eb1b9fffc9e55f91d90a796fa4e4c4917cd5b686
Peter Gnodde <peter@gnodde.org>
2020-09-02 17:08:25 +0200

    Support multiple days of month in DayOfMonth planning

--- planner/day_of_month.py
+++ planner/day_of_month.py
@@ -1,46 +1,56 @@
 import calendar
 import datetime
-from typing import Tuple
+from typing import List, Set, Tuple
 
 from .planning import Planning
 
-LAST_DAY_OF_MONTH = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+_LAST_DAY_OF_MONTH = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 
 class DayOfMonth(Planning):
-    def __init__(self, day: int):
+    def __init__(self, day: int, *days: int):
+        self.__days = {day, *days}
         assert 1 <= abs(day) <= 31
-        self.__day = day
 
     @property
-    def day(self) -> int:
-        return self.__day
+    def days(self) -> Set[int]:
+        return self.__days
 
     def match(self, date: datetime.datetime) -> bool:
-        return date.day == self.__get_day_of_month(date.year, date.month)
+        return date.day in self.__get_days_of_month(date.year, date.month)
 
     def next(self, date: datetime.datetime) -> datetime.datetime:
         year = date.year
         month = date.month
         day = date.day
 
-        while not day < self.__get_day_of_month(year, month) <= self.__get_last_day_of_month(year, month):
-            day = 0
-            year, month = self.__next_month(year, month)
+        while True:
+            days_of_month = self.__get_days_of_month(year, month)
+            while not days_of_month or day >= days_of_month[-1]:
+                year, month, day = self.__next_month(year, month)
+                days_of_month = self.__get_days_of_month(year, month)
 
-        return datetime.datetime(year=year, month=month, day=self.__get_day_of_month(year, month), tzinfo=date.tzinfo)
+            for dom in days_of_month:
+                if day < dom:
+                    return datetime.datetime(year=year, month=month, day=dom, tzinfo=date.tzinfo)
 
-    def __get_day_of_month(self, year: int, month: int) -> int:
-        if self.__day < 0:
-            return self.__get_last_day_of_month(year, month) + self.__day + 1
-        return self.__day
-
-    @staticmethod
-    def __get_last_day_of_month(year: int, month: int) -> int:
-        return LAST_DAY_OF_MONTH[month] + int(month == 2 and calendar.isleap(year))
+            year, month, day = self.__next_month(year, month)
 
     @staticmethod
-    def __next_month(year: int, month: int) -> Tuple[int, int]:
+    def __next_month(year: int, month: int) -> Tuple[int, int, int]:
         if month == 12:
-            return year + 1, 1
-        return year, month + 1
+            return year + 1, 1, 0
+        return year, month + 1, 0
+
+    def __get_days_of_month(self, year: int, month: int) -> List[int]:
+        days_of_month = set()
+        for dom in self.__days:
+            last_day_of_month = _LAST_DAY_OF_MONTH[month] + int(month == 2 and calendar.isleap(year))
+
+            if dom < 0:
+                dom = last_day_of_month + dom + 1
+
+            if 0 < dom <= last_day_of_month:
+                days_of_month.add(dom)
+
+        return sorted(days_of_month)
