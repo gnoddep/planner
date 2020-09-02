fd397ff1c17817f7f132aeb1c94ff81aaf4541f2
Peter Gnodde <peter@gnodde.org>
2020-09-02 17:26:22 +0200

    Assert all days of month are between 1 and 31

--- planner/day_of_month.py
+++ planner/day_of_month.py
@@ -10,7 +10,8 @@ _LAST_DAY_OF_MONTH = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 class DayOfMonth(Planning):
     def __init__(self, day: int, *days: int):
         self.__days = {day, *days}
-        assert 1 <= abs(day) <= 31
+        for day in self.__days:
+            assert 1 <= abs(day) <= 31
 
     @property
     def days(self) -> Set[int]:
